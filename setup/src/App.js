26010f757373d7ccd41672268e4e4c68b6fd4673
Flavio Tosi <tosi.flaviol@gmail.com>
2023-01-18 12:41:03 -0300

    added data and modified some files

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,26 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-
-const url = 'https://course-api.com/react-tabs-project'
-
+import data from './Data';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
-
-  const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newJobs = await response.json();
-    setJobs(newJobs);
-    setLoading(false);
-  };
+  const [value, setValue] = useState(0)
+  const [] = useState(data);
 
   useEffect(() => {
-    fetchJobs();
+    setLoading(false);
   }, []);
 
   if (loading) {
+    
     return (
       <section className='section loading'>
         <h1>loading</h1>
@@ -28,7 +20,7 @@ function App() {
     );
   }
 
-  const { company, dates, duties, title } = jobs[value];
+  const { company, dates, duties, title } = data[value];
   return (
     <section className='section'>
       <div className='title'>
@@ -36,15 +28,26 @@ function App() {
         <div className='underline'></div>
       </div>
       <div className='jobs-center'>
-        {/* */}
-        {/* */}
+        <div className='btn-container'>
+          {
+            data.map((item, index) => {
+              return (
+                <button key={item.id} onClick={() =>
+                  setValue(index)}
+                  className={`job-btn ${index === value && 'active-btn'}`}>
+                  {item.company}
+                </button>
+              )
+            })
+          }
+        </div>
         <article className='job-info'>
           <h3>{title}</h3>
           <h4>{company}</h4>
           <p className='job-date'>{dates}</p>
-          {duties.map((duty,index)=>{
+          {duties.map((duty, index) => {
             return (
-              <div key ={index} className='job-desc'>
+              <div key={index} className='job-desc'>
                 <FaAngleDoubleRight className='job-icon'>
 
                 </FaAngleDoubleRight>
